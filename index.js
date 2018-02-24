afd602ca70fb41d8a3470cd5d414cb7dbcfb6924
muhammadsayuti <muhammad.sayuti94@gmail.com>
2018-02-24 16:07:02 +0800

    update binding name

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var hasNative;
 var useJSDOM = !!process.env.USE_JSDOM;
 if (!useJSDOM) {
-	try {
-		hasNative = require.resolve(__dirname + '/lib/webkitgtk.node');
-	} catch(e) {}
+  try {
+    hasNative = require.resolve(__dirname + '/lib/webkit-gtk.node');
+  } catch (e) { }
 }
 var WebKit = require('./src/webkitgtk');
 if (!hasNative) {
-	var jsdom = require('jsdom');
-	if (!useJSDOM) {
-		console.info('No webkitgtk bindings available, trying with jsdom...');
-	}
-	if (!jsdom) {
-		console.info('jsdom is missing, please install it.\nExiting');
-		process.exit(1);
-	}
-	require('./jsdom')(WebKit);
+  var jsdom = require('jsdom');
+  if (!useJSDOM) {
+    console.info('No webkitgtk bindings available, trying with jsdom...');
+  }
+  if (!jsdom) {
+    console.info('jsdom is missing, please install it.\nExiting');
+    process.exit(1);
+  }
+  require('./jsdom')(WebKit);
 }
 
 module.exports = WebKit;
